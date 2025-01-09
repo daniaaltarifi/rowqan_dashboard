4fc8196d304c07e7aa4de4b98a2293a3a676275f
dania altarifi <dania.a.altarifi@gmail.com>
2025-01-09 09:12:44 +0300

    imported

--- src/SuperAdmin DashBoard/ReservationDetails.jsx
+++ src/SuperAdmin DashBoard/ReservationDetails.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
+
 
 function ReservationDetails() {
   const [reservations, setReservations] = useState([]);
